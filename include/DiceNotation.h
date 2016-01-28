a5e8bcbc67b135757c38911cf3e4fadf5551a40f
Mattias Cibien <mattias@mattiascibien.net>
2016-01-28 15:06:15 +0100

    Support GCC

--- include/DiceNotation.h
+++ include/DiceNotation.h
@@ -3,10 +3,21 @@
 #ifdef DICENOTATION_STATIC
 #	define DICENOTATION_API 
 #else
-#	ifdef DICENOTATION_EXPORTS
-#		define DICENOTATION_API _declspec(dllexport)
+#	if defined(_MSC_VER)
+#		ifdef DICENOTATION_EXPORTS
+#			define DICENOTATION_API _declspec(dllexport)
+#		else
+#			define DICENOTATION_API _declspec(dllimport)
+#		endif
+#	elif defined(_GCC)
+#		ifdef DICENOTATION_EXPORTS
+#			define DICENOTATION_API __attribute__((visibility("default")))
+#		else
+#			define DICENOTATION_API
+#		endif
 #	else
-#		define DICENOTATION_API _declspec(dllimport)
+#		define DICENOTATION_API
+#		pragma warning Unknown dynamic link import/export semantics.
 #	endif
 #endif
 
