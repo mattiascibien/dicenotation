09219556306291ea8d1a2230c70935a45990838c
Mattias Cibien <mattias@mattiascibien.net>
2018-04-20 16:58:11 +0200

    Added fake test

---
+++ tests/fake.cpp
@@ -0,0 +1,11 @@
+ #define BOOST_TEST_DYN_LINK
+
+ #define BOOST_TEST_MODULE "DiceNotationModule"
+
+#include <boost/test/unit_test.hpp>
+
+// ------------- Tests Follow --------------
+BOOST_AUTO_TEST_CASE( fake )
+{
+  BOOST_CHECK(1 == 1);
+}
